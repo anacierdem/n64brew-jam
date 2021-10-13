d8ea5e9e3272fdbce5f418a2ff9819f01967b597
Ali Naci Erdem <anacierdem@gmail.com>
2021-10-13 08:43:10 +0300

    Theroretical corrections, but is not working right
    - Convert everything to 16.16
    - Added related macros

--- src/main.c
+++ src/main.c
@@ -9,20 +9,48 @@ typedef struct {
 	int16_t y; // 11.2
 } RDPScreenVertex;
 
+
+#define make_16d16_2(A, frac)       (((A) << 16) | (uint16_t)(((frac) * 65535)))
+#define make_16d16_1(A)             (((A) << 16))
+
+#define GET_3TH_ARG(arg1, arg2, arg3, ...) arg3
+#define __make_16d16(...) GET_3TH_ARG(__VA_ARGS__, make_16d16_2, make_16d16_1)
+
+#define make_16d16(...) __make_16d16(__VA_ARGS__)(__VA_ARGS__)
+
+#define div_16d16(A, B) (((int64_t)(A) << 16) / (int64_t)(B))
+#define mult_16d16(A, B) (((int64_t)(A) * (int64_t)(B)) >> 16)
+#define frac_16d16(A) ((A) && 0xFFFF)
+#define from_16d16_to_11d2(A) ((int16_t)(A >> 14))
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+// #pragma message(STR(make_16d16(0, 0.25)))
+
+
 // Given vertices should be in correct order
-static void __attribute__ ((noinline)) render_tri(RdpDisplayList* rdl, int32_t v1x, int16_t v1y, int32_t v2x, int16_t v2y, int32_t v3x, int16_t v3y) {
-    int16_t     YL = v3y, YM = v2y, YH = v1y;
-    int32_t     DxLDy = ((v3y - v2y) == 0 ? 0 : (v3x - v2x) / ((int32_t)(v3y - v2y))) * 4,
-                DxHDy = ((v3y - v1y) == 0 ? 0 : (v3x - v1x) / ((int32_t)(v3y - v1y))) * 4,
-                DxMDy = ((v2y - v1y) == 0 ? 0 : (v2x - v1x) / ((int32_t)(v2y - v1y))) * 4,
-                XL = v2x, XH = v1x - ((DxHDy * (v1y && 0x3)) / 4) , XM = v1x - ((DxMDy * (v1y && 0x3) / 4));
+static void __attribute__ ((noinline)) render_tri(RdpDisplayList* rdl, int32_t v1x, int32_t v1y, int32_t v2x, int32_t v2y, int32_t v3x, int32_t v3y) {
+    int32_t     subpixel_height = make_16d16(0, 0.25);
+    int32_t     YL = v3y,
+                YM = v2y,
+                YH = v1y,
+                DxLDy = (v3y - v2y) == 0 ? 0 : div_16d16(v3x - v2x, v3y - v2y),
+                DxHDy = (v3y - v1y) == 0 ? 0 : div_16d16(v3x - v1x, v3y - v1y),
+                DxMDy = (v2y - v1y) == 0 ? 0 : div_16d16(v2x - v1x, v2y - v1y),
+                XL = v2x + mult_16d16(DxLDy, subpixel_height - (v2y && subpixel_height) ),
+                XH = v1x - mult_16d16(DxHDy, frac_16d16(v1y)),
+                XM = v1x - mult_16d16(DxMDy, frac_16d16(v1y));
 
+            // - (DxHDy * (v1y && 0x3) / 4)
+            // - (DxMDy * (v1y && 0x3) / 4)
+    debugf("start DxLDy: %ld, DxHDy: %ld, DxMDy: %ld, T: %08X \n", DxLDy, DxHDy, DxMDy, make_16d16(0, 0.25));
 
-            // - DxHDy * (v1y && 0x3)
-            // - DxMDy * (v1y && 0x3)
-    // debugf("start DxLDy: %ld, DxHDy: %ld, DxMDy: %ld\n", DxLDy, DxHDy, DxMDy);
-
-    rdl_push(rdl, (cast64(0xC8) << 56) | (cast64(1) << 55) | (cast64(YL) << 32) | (cast64(YM) << 16) | (cast64(YH) << 0) );
+    rdl_push(rdl, (cast64(0xC8) << 56) | (cast64(1) << 55) |
+        (cast64(from_16d16_to_11d2(YL)) << 32) |
+        (cast64(from_16d16_to_11d2(YM)) << 16) |
+        (cast64(from_16d16_to_11d2(YH)) << 0)
+    );
     rdl_push(rdl, (cast64(XL) << 32) | (cast64(DxLDy) << 0) );
     rdl_push(rdl, (cast64(XH) << 32) | (cast64(DxHDy) << 0) );
     rdl_push(rdl, (cast64(XM) << 32) | (cast64(DxMDy) << 0) );
@@ -49,7 +77,9 @@ int main(void)
 
     static display_context_t disp = 0;
 
-    int left = 0;
+    int32_t left = 0;
+
+    RdpDisplayList *rdl = rdl_heap_alloc(100);
 
     while(1) {
 
@@ -57,10 +87,8 @@ int main(void)
 
         rdp_attach_display( disp );
 
+        rdl_reset(rdl);
 
-        RdpDisplayList *rdl = rdl_heap_alloc(100);
-
-        
         rdl_push(rdl,RdpSyncPipe());
         rdl_push(rdl,RdpSetClippingI(0, 0, 640, 240));
         rdl_push(rdl,RdpSetOtherModes(SOM_CYCLE_FILL));
@@ -69,7 +97,7 @@ int main(void)
         rdl_push(rdl,RdpFillRectangleI(0, 0, 640, 240));
 
         rdl_push(rdl,RdpSetFillColor(RDP_COLOR16(31,0,0,1)));
-        rdl_push(rdl,RdpFillRectangleI(left, left, left + 100, left + 50));
+        rdl_push(rdl,RdpFillRectangleI(0, 0, 0 + 100, 0 + 50));
 
         rdl_push(rdl,RdpSyncPipe());
 
@@ -96,18 +124,18 @@ int main(void)
 
 
         render_tri(rdl,
-            (50 << 16),    (0 << 2),
-            (150 << 16),  (25 << 2),
-            (50 << 16),   (50 << 2)
+            left + make_16d16(50),   left + make_16d16(0),
+            left + make_16d16(150),  left + make_16d16(25),
+            left + make_16d16(50),   left + make_16d16(50)
         );
 
 
         rdl_push(rdl,RdpSetFogColor(cast64(RDP_COLOR32(255,0,255,50))));
 
         render_tri(rdl,
-            (50 << 16),    (0 << 2),
-            (250 << 16),  (10 << 2),
-            (150 << 16),   (25 << 2)
+            make_16d16(50),    make_16d16(0),
+            make_16d16(250),   make_16d16(10),
+            make_16d16(150),   make_16d16(25)
         );
         // rdl_push(rdl,RdpSetCombine(Comb1_Rgb(ZERO, ZERO, ZERO, PRIM), Comb1_Alpha(ZERO, ZERO, ZERO, PRIM)));
 
@@ -117,9 +145,9 @@ int main(void)
         // Present
         rdp_detach_display();
         display_show(disp);
-        wait_ms(1);
+        wait_ms(17);
 
-        left += 1;
+        left += make_16d16(0, 0.01);
 
         // while(1) {};
     }
