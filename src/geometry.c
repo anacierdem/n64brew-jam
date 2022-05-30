a3c3f2076b3b161f54e43ccfbcacd3f27f1fde7d
Ali Naci Erdem <anacierdem@gmail.com>
2022-05-30 08:14:47 +0300

    Update gfx branch

--- src/geometry.c
+++ src/geometry.c
@@ -44,7 +44,7 @@ static inline void __render_tri(int32_t x1, int32_t y1, int32_t x2, int32_t y2, 
         (int64_t)(y3 - y1) * (int64_t)(x2 - x1)
     ) > 0 ? 0 : 1;
 
-    rdpq_fill_triangle(dir, 0, 0,
+    rdpq_fill_triangle(dir,
         from_16d16_to_11d2(YL),
         from_16d16_to_11d2(YM),
         from_16d16_to_11d2(YH),
